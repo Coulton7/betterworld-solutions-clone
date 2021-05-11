5202d4ee3a130b53beb2c15395d6a23d5712a16d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-11 13:32:17 +0100

    Uploading new js updates

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,20 +1,59 @@
 (function($) {
-    
+
   var $window = $(window),
   flexslider = { vars:{} };
 
   function getGridSize() {
     return (window.innerWidth < 768) ? 2 :
            (window.innerWidth < 992) ? 3 :
-           (window.innerWidth < 1200) ? 3 : 3;
+           (window.innerWidth < 1200) ? 4 : 4;
   }
-    
+
+  var TxtType = function(el, toRotate, period) {
+          this.toRotate = toRotate;
+          this.el = el;
+          this.loopNum = 0;
+          this.period = parseInt(period, 10) || 7000;
+          this.txt = '';
+          this.tick();
+      };
+
+      TxtType.prototype.tick = function() {
+          var i = this.loopNum % this.toRotate.length;
+          var fullTxt = this.toRotate[i];
+
+          if (this.isDeleting) {
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+          } else {
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+          }
+
+          this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+          var that = this;
+          var delta = 100 - Math.random() * 100;
+
+          if (this.isDeleting) { delta /= 2; }
+
+          if (!this.isDeleting && this.txt === fullTxt) {
+          delta = this.period;
+          this.isDeleting = false;
+          } else if (this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+          this.loopNum==1;
+          delta = 500;
+          }
+
+          setTimeout(function() {
+          that.tick();
+          }, delta);
+      };
+
   $(document).ready(function() {
 
     $('.flexslider').flexslider({
       controlNav: false,
-      directionNav: false,
-      useCSS: false
+      directionNav: false
 
     });
 
@@ -26,12 +65,11 @@
     $('.carousel-slider').flexslider({
       controlNav: false,
       directionNav: true,
-      slideshowSpeed: 4000,
       animation: "slide",
       easing: "swing",
       animationLoop: true,
       pauseOnHover: true,
-      itemWidth: 210,
+      itemWidth: 180,
       itemMargin: 15,
       minItems: getGridSize(),
       maxItems: getGridSize(),
@@ -39,6 +77,36 @@
       start: function(slider){
         flexslider = slider;
       }
+    });
+
+    $('.newsslider').flexslider({
+      controlNav: false,
+      directionNav: false,
+      start: function(slider){
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+      },
+      after: function(slider){
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+      },
+      animation: "fade",
+      slideshowSpeed: 12000,
+      easing: "swing",
+      animationLoop: true,
+
     });
 
     $('.product-slider').flexslider({
@@ -54,13 +122,18 @@
       directionNav: true
     });
 
+    $('.base-slider').flexslider({
+      controlNav: false,
+      directionNav: false
+    });
+
   });
-    
-    $window.resize(function(){
+
+  $window.resize(function(){
     var gridSize = getGridSize();
 
     flexslider.vars.minItems = gridSize;
     flexslider.vars.maxItems = gridSize;
   });
-    
-})(jQuery)
+
+})(jQuery);
