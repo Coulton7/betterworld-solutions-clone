13f9396ea264527d8a00b359837735910e141177
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-12 13:45:05 +0100

    New method of handling headlines

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,46 +9,6 @@
            (window.innerWidth < 1200) ? 4 : 4;
   }
 
-  var TxtType = function(el, toRotate, period) {
-          this.toRotate = toRotate;
-          this.el = el;
-          this.loopNum = 0;
-          this.period = parseInt(period, 10) || 7000;
-          this.txt = '';
-          this.tick();
-      };
-
-      TxtType.prototype.tick = function() {
-          var i = this.loopNum % this.toRotate.length;
-          var fullTxt = this.toRotate[i];
-
-          if (this.isDeleting) {
-          this.txt = fullTxt.substring(0, this.txt.length - 1);
-          } else {
-          this.txt = fullTxt.substring(0, this.txt.length + 1);
-          }
-
-          this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-          var that = this;
-          var delta = 100 - Math.random() * 100;
-
-          if (this.isDeleting) { delta /= 2; }
-
-          if (!this.isDeleting && this.txt === fullTxt) {
-          delta = this.period;
-          this.isDeleting = false;
-          } else if (this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.loopNum==1;
-          delta = 500;
-          }
-
-          setTimeout(function() {
-          that.tick();
-          }, delta);
-      };
-
   $(document).ready(function() {
 
     $('.flexslider').flexslider({
@@ -83,27 +43,38 @@
       controlNav: false,
       directionNav: false,
       start: function(slider){
-        var elements = document.querySelectorAll('.flex-active-slide .typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
+        var curSlide = slider.find("li.flex-active-slide");
+        var content = ($(curSlide, id).attr("data-type"));
+        var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+        $(ele).hide().appendTo($('.flex-active-slide p.typewrite')).each(function (i) {
+            $(this).delay(50 * i).css({
+                display: 'inline',
+                opacity: 0
+            }).animate({
+                opacity: 1
+            }, 50);
+        });
       },
       after: function(slider){
-        var elements = document.querySelectorAll('.flex-active-slide .typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
+        var curSlide = slider.find("li.flex-active-slide");
+        var content = ($(curSlide, id).attr("data-type"));
+
+        console.log(content);
+
+        var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+        $(ele).hide().appendTo($('.flex-active-slide p.typewrite')).each(function (i) {
+            $(this).delay(50 * i).css({
+                display: 'inline',
+                opacity: 0
+            }).animate({
+                opacity: 1
+            }, 50);
+        });
       },
       animation: "fade",
-      slideshowSpeed: 10000,
+      slideshowSpeed: 12000,
       easing: "swing",
       animationLoop: true,
 
