4ec0fb96ac0d303e4c17e30b71bce5e5a0422067
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-18 10:34:12 +0100

    Adding the counter js

---
+++ assets/js/counter.js
@@ -0,0 +1,28 @@
+(function($){
+  document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+      useEasing: true,
+      useGrouping: true,
+      separator: ',',
+      decimal: '.',
+    };
+
+    var count = new CountUp('car-counter', 0, 529500000, 0, 6,  options);
+
+    jQuery(window).on("resize scroll load", function(){
+      var scrollPosition = jQuery(window).scrollTop();
+      var viewportHeight = window.innerHeight;
+      var offset = jQuery("#car-counter").offset();
+      var elementHeight = jQuery("#car-counter").height();
+      if(offset.top > scrollPosition - elementHeight && offset.top < scrollPosition + viewportHeight) {
+        if (!count.error) {
+          count.start();
+        } else {
+          console.error(count.error);
+        }
+      } else {
+        return false;
+      }
+    })
+  });
+})(jQuery);
